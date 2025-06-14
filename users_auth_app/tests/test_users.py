100e3a72686d2ab8235b2b3cbde6e0ade744da51
LauraHexx <l.hesidenz97@gmail.com>
2025-06-14 09:44:50 +0200

    added: tests authentication

--- users_auth_app/tests/test_users.py
+++ users_auth_app/tests/test_users.py
@@ -2,6 +2,8 @@
 import pytest
 import uuid
 from users_auth_app.models import CustomUser
+from users_auth_app.api.backends import VerifiedEmailBackend
+from utils.test_utils import create_verified_user, create_unverified_user
 
 
 def create_regular_user(email="user@example.com", password="securepass123", username="testuser"):
@@ -63,3 +65,45 @@ def test_verification_token_auto_assigned():
     user = create_regular_user()
     assert user.verification_token is not None
     assert isinstance(user.verification_token, uuid.UUID)
+
+
+@pytest.mark.django_db
+def test_authenticate_success():
+    """Authenticate returns user if email, password and is_verified are correct."""
+    user, password = create_verified_user()
+    backend = VerifiedEmailBackend()
+    authenticated_user = backend.authenticate(
+        request=None, email=user.email, password=password)
+
+    assert authenticated_user == user
+
+
+@pytest.mark.django_db
+def test_authenticate_wrong_email():
+    """Authenticate returns None if email does not exist."""
+    backend = VerifiedEmailBackend()
+    authenticated_user = backend.authenticate(
+        request=None, email="wrong@example.com", password="secret123")
+    assert authenticated_user is None
+
+
+@pytest.mark.django_db
+def test_authenticate_wrong_password():
+    """Authenticate returns None if password is wrong."""
+    user, _ = create_verified_user()
+    backend = VerifiedEmailBackend()
+    authenticated_user = backend.authenticate(
+        request=None, email=user.email, password="wrongpass")
+
+    assert authenticated_user is None
+
+
+@pytest.mark.django_db
+def test_authenticate_not_verified():
+    """Authenticate returns None if user is not verified."""
+    user, password = create_unverified_user()
+    backend = VerifiedEmailBackend()
+    authenticated_user = backend.authenticate(
+        request=None, email=user.email, password=password)
+
+    assert authenticated_user is None
