d740632b8d8c0c3fe070bf357eaf66a17c3b400b
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2025-06-21 08:03:58 +0200

    fix: resolution video 3s compatible

--- video_flix_app/api/serializers.py
+++ video_flix_app/api/serializers.py
@@ -1,6 +1,35 @@
 from rest_framework import serializers
 from django.conf import settings
+import boto3
+from botocore.exceptions import ClientError
 from ..models import Video, UserWatchHistory, VideoResolution
+
+
+def get_s3_client():
+    """Get configured S3/MinIO client."""
+    return boto3.client(
+        's3',
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_S3_REGION_NAME,
+        use_ssl=settings.AWS_S3_USE_SSL,
+        verify=settings.AWS_S3_VERIFY
+    )
+
+
+def generate_presigned_url(s3_key, expiration=3600):
+    """Generate a presigned URL for S3 object."""
+    try:
+        s3_client = get_s3_client()
+        response = s3_client.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': s3_key},
+            ExpiresIn=expiration
+        )
+        return response
+    except ClientError:
+        return None
 
 
 class VideoResolutionSerializer(serializers.ModelSerializer):
@@ -12,9 +41,9 @@ class VideoResolutionSerializer(serializers.ModelSerializer):
         read_only_fields = ['height', 'file']
 
     def get_file_url(self, obj):
-        """Generate full S3 URL for video file."""
+        """Generate presigned S3 URL for video file."""
         if obj.file:
-            return f"{settings.MEDIA_URL}{obj.file}"
+            return generate_presigned_url(obj.file)
         return None
 
 
@@ -35,9 +64,9 @@ class VideoSerializer(serializers.ModelSerializer):
         ]
 
     def get_thumbnail_url(self, obj):
-        """Generate full S3 URL for thumbnail."""
+        """Generate presigned S3 URL for thumbnail."""
         if obj.thumbnail:
-            return f"{settings.MEDIA_URL}{obj.thumbnail}"
+            return generate_presigned_url(obj.thumbnail)
         return None
 
 
